524cbce511e300d4e47c2fa7c97ed6411d1d6fe9
srianbury <bsunbury29@gmail.com>
2022-07-11 17:21:14 -0700

    mui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,17 @@
 // https://nextjs.org/docs/advanced-features/custom-app
+import NextHead from "next/head";
+import { CssBaseline } from "@mui/material";
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextHead>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </NextHead>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export { App };
