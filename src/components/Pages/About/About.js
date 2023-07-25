6c1468c87c3aea14a8caf77a86e1ddcc68afb047
srianbury <bsunbury29@gmail.com>
2023-07-24 22:26:44 -0700

    init about

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Box } from "@mui/material";
 
 function About() {
   return (
@@ -7,7 +8,16 @@ function About() {
         <title>Workout | About</title>
       </Head>
       <main>
-        <h1>About</h1>
+        <h1>About this Platform</h1>
+        <Box>
+          <h2>
+            Platform (name TBD) is a place to share and find workouts and
+            fitness related content. It&#39;s still in its early stages and I
+            appriciate you checking it out and while I work on getting it past
+            an MVP! You can share your thoughts, questions, or anything else
+            with me at bsunbury29@gmail.com :)
+          </h2>
+        </Box>
       </main>
     </div>
   );
